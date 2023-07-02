76ed662d56eaeaa8ea3d0945b1706f6139868087
YuliaPortnova <yuyul@yandex.ru>
2023-07-02 22:49:23 +0300

    Публикует окончательную версию сайта

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,1 @@
-const mainHeaderToggle = document.querySelector('.main-header__toggle');
-const mainНeader = document.querySelector('.main-header');
-
-mainНeader.classList.remove('no-js');
-
-mainHeaderToggle.addEventListener('click', () => mainНeader.classList.toggle('is-open'));
+const mainHeaderToggle=document.querySelector(".main-header__toggle"),mainНeader=document.querySelector(".main-header"),contactsMapWrapper=document.querySelector(".contacts__map-wrapper");mainНeader.classList.remove("no-js"),contactsMapWrapper.classList.remove("no-js"),mainHeaderToggle.addEventListener("click",(()=>mainНeader.classList.toggle("is-open")));
